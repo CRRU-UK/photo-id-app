c00698254786d4a3db1471c3b8f862fb1818e44b
samwisekind <samuel.kindler@icloud.com>
2025-09-08 18:54:48 +0100

    Update

--- src/backend/recents.ts
+++ src/backend/recents.ts
@@ -16,8 +16,6 @@ const getRecentProjects = async (): Promise<RecentProject[]> => {
     return [];
   }
 
-  debugger;
-
   const data = await fs.promises.readFile(recentProjectsFile, "utf8");
   return JSON.parse(data) as RecentProject[];
 };
