4d86e057e16b7d19815de8eda9b9b0919dd1eb4c
samwisekind <samuel.kindler@icloud.com>
2025-09-10 11:50:00 +0100

    Fix Sharp build

--- forge.config.ts
+++ forge.config.ts
@@ -49,6 +49,13 @@ const config: ForgeConfig = {
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
+    {
+      name: "@timfish/forge-externals-plugin",
+      config: {
+        externals: ["sharp"],
+        includeDeps: true,
+      },
+    },
   ],
   hooks: {
     generateAssets: async () => {
