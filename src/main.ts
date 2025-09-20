32b7def3c334e168521277d5c3a31d54d0df1c2b
samwisekind <samuel.kindler@icloud.com>
2025-09-20 19:45:14 +0100

    Fix external links

--- src/main.ts
+++ src/main.ts
@@ -268,10 +268,6 @@ app.whenReady().then(() => {
       shell.openExternal(EXTERNAL_LINKS.USER_GUIDE);
     }
 
-    if (link === "user-guide") {
-      shell.openExternal(EXTERNAL_LINKS.USER_GUIDE);
-    }
-
     if (link === "changelog") {
       shell.openExternal(EXTERNAL_LINKS.CHANGELOG);
     }
