4b0d8c1ce30e170eb464c4841435faf179a6d6bc
samwisekind <samuel.kindler@icloud.com>
2026-02-11 17:13:51 +0000

    Updates

--- src/constants.ts
+++ src/constants.ts
@@ -188,8 +188,8 @@ export const DEFAULT_PHOTO_EDITS: PhotoEdits = {
 
 export const ZOOM_FACTORS = {
   BUTTON: 1.2,
-  WHEEL_PIXEL: 1.02, // Trackpad
-  WHEEL_LINE: 1.08, // Mouse wheel
+  WHEEL_DELTA_PIXEL_FACTOR: 1.02, // Trackpad
+  WHEEL_DELTA_LINE_FACTOR: 1.08, // Mouse wheel
 };
 
 export const PAN_AMOUNT = 50;
--- src/frontend/hooks/imageEditor/useZoomInteraction.ts
+++ src/frontend/hooks/imageEditor/useZoomInteraction.ts
@@ -53,9 +53,11 @@ export const useZoomInteraction = ({
       const imagePointX = (imageCoords.x - centreX - pan.x) / zoom + centreX;
       const imagePointY = (imageCoords.y - centreY - pan.y) / zoom + centreY;
 
-      const isPixelScroll = event.deltaMode === WheelEvent.DOM_DELTA_PIXEL;
-      const wheelFactor = isPixelScroll ? ZOOM_FACTORS.WHEEL_PIXEL : ZOOM_FACTORS.WHEEL_LINE;
-      const delta = event.deltaY > 0 ? 1 / wheelFactor : wheelFactor;
+      const isPixelDelta = event.deltaMode === WheelEvent.DOM_DELTA_PIXEL;
+      const wheelFactorByDeltaMode = isPixelDelta
+        ? ZOOM_FACTORS.WHEEL_DELTA_PIXEL_FACTOR
+        : ZOOM_FACTORS.WHEEL_DELTA_LINE_FACTOR;
+      const delta = event.deltaY > 0 ? 1 / wheelFactorByDeltaMode : wheelFactorByDeltaMode;
 
       const newZoom = zoom * delta;
       const updatedZoom = Math.max(newZoom, 1);
