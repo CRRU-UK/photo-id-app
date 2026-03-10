243b69843be00780cef4c0c0792e90f3f2fd41f5
samwisekind <samuel.kindler@icloud.com>
2026-03-10 16:53:49 +0000

    Revert

--- forge.config.ts
+++ forge.config.ts
@@ -41,19 +41,19 @@ const config: ForgeConfig = {
       build: [
         {
           entry: "src/main.ts",
-          config: "config/vite.main.config.mts",
+          config: "vite.main.config.mts",
           target: "main",
         },
         {
           entry: "src/preload.ts",
-          config: "config/vite.preload.config.mts",
+          config: "vite.preload.config.mts",
           target: "preload",
         },
       ],
       renderer: [
         {
           name: "main_window",
-          config: "config/vite.renderer.config.mts",
+          config: "vite.renderer.config.mts",
         },
       ],
     }),
