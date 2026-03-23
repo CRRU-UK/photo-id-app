a121ade4881a04dcedd44595f86a610add009512
samwisekind <samuel.kindler@icloud.com>
2026-03-23 12:30:22 +0000

    Add DNS resource

---
+++ terraform/dns.tf
@@ -0,0 +1,9 @@
+resource "cloudflare_dns_record" "dns_cname" {
+  zone_id = var.cloudflare_zone_id
+
+  name    = "photoidapp"
+  type    = "CNAME"
+  content = "crru-uk.github.io"
+  ttl     = "1"
+  comment = "Photo ID App docs (GitHub Pages)"
+}
