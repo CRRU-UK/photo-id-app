4a020a798962f989e683f27cef45e57c7c08b60b
samwisekind <samuel.kindler@icloud.com>
2026-04-02 11:35:03 +0100

    Add ruleset for docs site

---
+++ terraform/cloudflare.tf
@@ -0,0 +1,32 @@
+resource "cloudflare_dns_record" "dns_cname" {
+  zone_id = var.cloudflare_zone_id
+
+  name    = "photoidapp"
+  type    = "CNAME"
+  content = "crru-uk.github.io"
+  ttl     = "1"
+  proxied = true
+  comment = "Photo ID App docs (GitHub Pages)"
+}
+
+# Rewrite for GitHub Pages to prevent redirect loops with Cloudflare SSL config
+resource "cloudflare_ruleset" "docs_https_redirect" {
+  zone_id = var.cloudflare_zone_id
+
+  name        = "Disable HTTPS enforcement for documentation"
+  description = "Skip Cloudflare enforcement to avoid redirect loop with GitHub Pages"
+  kind        = "zone"
+  phase       = "http_config_settings"
+
+  rules = [{
+    enabled = true
+
+    expression  = "(http.host eq \"photoidapp.crru.org.uk\")"
+    description = "Disable Automatic HTTPS Rewrites for GitHub Pages subdomain"
+
+    action = "set_config"
+    action_parameters = {
+      automatic_https_rewrites = false
+    }
+  }]
+}
